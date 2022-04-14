2ea6367bf6e0365e8b95da966897353c525aef7f
janducreations <janducreations@gmail.com>
2022-04-14 08:01:59 +0000

    initiak setup

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/my-project-latest/main.cc" "/workspace/my-project-latest/CMakeFiles/main.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  "TBB_DO_ASSERT=1"
+  "TBB_USE_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/dealii/dealii-v8.5.1/include"
+  "/home/dealii/dealii-v8.5.1/include/deal.II/bundled"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include"
+  "/usr/lib/openmpi/include/openmpi"
+  "/home/dealii/libs/parmetis-4.0.3/include"
+  "/home/dealii/libs/trilinos-release-12-10-1/include"
+  "/home/dealii/libs/superlu_dist_5.1.2/include"
+  "/home/dealii/libs/petsc-3.6.4/include"
+  "/home/dealii/libs/hdf5-1.8.15-patch1/include"
+  "/home/dealii/libs/oce.git/include/oce"
+  "/home/dealii/libs/p4est-2.0/FAST/include"
+  "/home/dealii/libs/slepc-3.6.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
